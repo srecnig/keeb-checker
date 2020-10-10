edeeb53d2d97b9a9bb2d2b838586236ae2ad8bbd
martin sereinig <martin@veryunited.net>
2020-10-10 16:18:23 +0200

    more basic setup

--- src/App.js
+++ src/App.js
@@ -4,26 +4,51 @@ import useEventListener from "@use-it/event-listener";
 
 import "./App.css";
 
-const useKeyDown = () => {
-  const [keyPress, setKeyPress] = useState(["", ""]);
+const useDetectKeypress = () => {
+  const [keyPress, setKeyPress] = useState([null, null]);
 
   useEventListener("keydown", ({ code, key }) => {
     setKeyPress([code, key]);
   });
 
+  useEventListener("keyup", ({ code, key }) => {
+    setKeyPress([null, null]);
+  });
+
   return keyPress;
 };
 
+function KeyPress({code, value}) {
+  return (
+    <React.Fragment>
+      <div className="keypress">{code}</div>
+      <div className="keypress">{value}</div>
+    </React.Fragment>
+  )
+}
+
+function Key({ label, size = 1, status = "pristine" }) {
+  const className = `key key--${status}`
+  return (
+    <div className={className}>{label}</div>
+  )
+}
+
 function App() {
-  const [code, key] = useKeyDown();
+  const [code, key] = useDetectKeypress();
 
   return (
     <div className="App">
-      <p>
-        OHAI MARK: {code},{key}
-      </p>
+        <KeyPress code={code} value={key} />
+        <hr />
+        <Key label="W" />
+        <Key label="A" />
+        <Key label="S" />
+        <Key label="D" />
     </div>
   );
 }
 
 export default App;
+
+
