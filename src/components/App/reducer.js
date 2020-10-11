fdc562081c7b9277d208b6f8032215b1b4268346
martin sereinig <martin@veryunited.net>
2020-10-11 10:03:56 +0200

    bugfix: only set status of keycodes that exists

--- src/components/App/reducer.js
+++ src/components/App/reducer.js
@@ -1,10 +1,11 @@
 import { KeyStatus, KeyCodes } from "./constants";
 
+const initialKeysState = Object.entries(KeyCodes).reduce(
+  (acc, [_, keyCode]) => ({ ...acc, [keyCode]: KeyStatus.PRISTINE }),
+  {}
+)
 const initialState = {
-  keys: Object.entries(KeyCodes).reduce(
-    (acc, [_, keyCode]) => ({ ...acc, [keyCode]: KeyStatus.PRISTINE }),
-    {}
-  ),
+  keys: initialKeysState
 };
 
 export function reducer(state = initialState, action) {
@@ -12,29 +13,31 @@ export function reducer(state = initialState, action) {
 
   switch (action.type) {
     case "SET_KEY_ACTIVE":
-      return {
-        keys: {
-          ...state.keys,
-          [action.keyCode]: KeyStatus.ACTIVE,
-        },
-      };
+      return updateInState(state, action.keyCode, KeyStatus.ACTIVE)
     case "SET_KEY_WORKING":
-      return {
-        keys: {
-          ...state.keys,
-          [action.keyCode]: KeyStatus.WORKING,
-        },
-      };
+      return updateInState(state, action.keyCode, KeyStatus.WORKING)
     case "RESET_KEY":
-      return {
-        keys: {
-          ...state.keys,
-          [action.keyCode]: KeyStatus.RESET,
-        },
-      };
+      return updateInState(state, action.keyCode, KeyStatus.RESETs)
     case "RESET_PRISTINE":
-      return initialState;
+      return {
+        ...state,
+        keys: initialKeysState,
+      };
     default:
       return state;
+  }
+}
+
+function updateInState(state, keyCode, status) {
+  if (!state.keys[keyCode]) {
+    return state
+  } else {
+    return {
+      ...state,
+      keys: {
+        ...state.keys,
+        [keyCode]: status,
+      },
+    };
   }
 }
